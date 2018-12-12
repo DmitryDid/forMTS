62aa4b5032b2d5d72316844c07b870ee2630c66e
DmitryDid <46Lulddf846tft6s>
2018-12-12 23:57:16 +0700

    added tests. version 1.3

--- src/main/java/dns/tests/dnsTest.java
+++ src/main/java/dns/tests/dnsTest.java
@@ -23,6 +23,38 @@ public class dnsTest extends Core {
     }
 
     @Test
+    public void sorting_zero_item() {
+        logger.logInfo("Подготовка данных для теста:");
+
+        menu.selectCategory("Ноутбуки и планшеты");
+        menu.selectCategory("Ноутбуки и аксессуары");
+        menu.selectSubCategory("Ноутбуки");
+
+        filter.fillMinPrice("1000");
+        filter.fillMaxPrice("1000");
+        filter.selectProducer("ASUS");
+        filter.apply();
+
+        checkCatalogSort();
+    }
+
+    @Test
+    public void sorting_many_item() {
+        logger.logInfo("Подготовка данных для теста:");
+
+        menu.selectCategory("Ноутбуки и планшеты");
+        menu.selectCategory("Ноутбуки и аксессуары");
+        menu.selectSubCategory("Ноутбуки");
+
+        filter.fillMinPrice("10000");
+        filter.fillMaxPrice("150000");
+        filter.selectProducer("LG");
+        filter.apply();
+
+        checkCatalogSort();
+    }
+
+    @Test
     public void sorting_into_other_category() {
         logger.logInfo("Подготовка данных для теста:");
 
@@ -31,7 +63,7 @@ public class dnsTest extends Core {
         menu.selectSubCategory("Телевизоры");
 
         filter.fillMinPrice("25000");
-        filter.fillMaxPrice("30000");
+        filter.fillMaxPrice("50000");
         filter.selectProducer("LG");
         filter.apply();
 
